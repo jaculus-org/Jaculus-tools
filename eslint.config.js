b9313a63d7d582155aa1474e7a4440f72c8a9b78
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-08-23 03:12:57 +0200

    feat: add Prettier and ESLint configurations, update package scripts, and create pre-commit hook, edit packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: { project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'] }
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: { project: ["./apps/*/tsconfig.json", "./packages/*/tsconfig.json"] },
+        },
+        plugins: { "@typescript-eslint": tseslint.plugin },
+        rules: {
+            indent: ["error", 4],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+            "@typescript-eslint/no-explicit-any": "off",
+        },
     },
-    plugins: { '@typescript-eslint': tseslint.plugin },
-    rules: {
-      indent: ['error', 4],
-      'linebreak-style': ['error', 'unix'],
-      quotes: ['error', 'double'],
-      semi: ['error', 'always'],
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
-  }
+    eslintConfigPrettier
 );
